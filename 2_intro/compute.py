7599e5bf444f2ed188177b9d77d6b7ecd1e43346
James Harding <jt.harding70@gmail.com>
2022-09-27 06:27:03 +0000

    Chapter 2 thru 3

---
+++ 2_intro/compute.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+a = tf.constant(10)
+print(a)
+print(type(a))
+b = tf.constant(32)
+print(b)
+print(a+b)
---
+++ 2_intro/where_is_tf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python -c 'import os,tensorflow;print(os.path.dirname(tensorflow.__file__))'
